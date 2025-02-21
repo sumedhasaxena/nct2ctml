c255d2fb916518d76b99c70e424ccfee33890648
Sumedha <sumedha.saxena@gmail.com>
2025-02-21 18:34:03 +0800

    add method to convert the result from mapping of clinical criteria to ctml format

--- match_criteria_mapper.py
+++ match_criteria_mapper.py
@@ -8,3 +8,19 @@ def get_keywords_from_conditions(conditions_list):
             if cond_keyword.lower() not in config.keywords_to_remove:
                 all_keywords.add(cond_keyword)
     return all_keywords
+
+def convert_to_ctml_clinical_schema(clinical_critera) -> dict:
+    result = {"or": []}
+    
+    # Extract the diagnosis list
+    diagnoses = clinical_critera.pop("oncotree_primary_diagnosis")
+    
+    for diagnosis in diagnoses:
+        clinical_entry = {
+            "clinical": {
+                **clinical_critera,  # Duplicate all other keys
+                "oncotree_primary_diagnosis": diagnosis  # Add the current diagnosis
+            }
+        }
+        result["or"].append(clinical_entry)    
+    return result
