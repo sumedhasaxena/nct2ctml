9973d256dfd833fe9a16af46b90d340c04144fa4
Sumedha <sumedha.saxena@gmail.com>
2025-04-09 15:18:59 +0800

    Fix for correctly structuring the clinical and genomic criterias as returned by AI

--- match_criteria_mapper.py
+++ match_criteria_mapper.py
@@ -1,5 +1,6 @@
 import config
 import aho_corasick as ac
+import trial_data_helper as tdh
 
 def get_keywords_from_conditions(conditions_list):
     all_keywords = set()
@@ -15,15 +16,24 @@ def convert_to_ctml_clinical_schema(clinical_critera) -> dict:
     diagnoses = clinical_critera.pop("oncotree_primary_diagnosis")
 
     if diagnoses and len(diagnoses) > 1: #incase of multiple diagnoses, put the result under 'or' operator
-        result = {"or": []}    
+        
+        result = {"and":[]}
+        diagnosis_result = {"or": []}
         for diagnosis in diagnoses:
-            clinical_ctml = {
-                "clinical": {
-                    **clinical_critera,  # Duplicate all other keys
-                    "oncotree_primary_diagnosis": diagnosis  # Add the current diagnosis
+            diagnosis_ctml = {
+                "clinical": {                    
+                    "oncotree_primary_diagnosis": diagnosis
                 }
             }
-            result["or"].append(clinical_ctml)
+            diagnosis_result["or"].append(diagnosis_ctml)        
+        
+        other_clinical_ctml = {
+                "clinical": {
+                    **clinical_critera
+                }
+            }
+        result["and"].append(diagnosis_result)
+        result["and"].append(other_clinical_ctml)
         return result
     else:         
         clinical_ctml = {
@@ -34,9 +44,16 @@ def convert_to_ctml_clinical_schema(clinical_critera) -> dict:
             }
         result = clinical_ctml
         return result
+
+# Checks that the genomic crietria returned by AI model is not empty and has "hugo_symbol", "variant_category" keys
+def convert_to_ctml_genomic_schema(genomic_criteria: dict) -> dict: 
+    if genomic_criteria and all(key in tdh.get_all_keys(genomic_criteria) for key in ["hugo_symbol", "variant_category"]):
+        #post processing
+        genomic_criteria = tdh.update_hugo_symbol(genomic_criteria)
+        return genomic_criteria
+    return {}
     
-def convert_to_ctml_genomic_schema(genomic_critera) -> dict:      
-    return genomic_critera #TODO: check if needs formatting
+
 
 def combine_clinical_and_genomic_ctml(clinical_ctml, genomic_ctml):
     if genomic_ctml and len(genomic_ctml) > 0:
