5fae85ee8b4ac3a0dd38cf6cf9fb0b3584a9f977
Sumedha <sumedha.saxena@gmail.com>
2025-04-09 15:14:54 +0800

    Add more config params

--- config.py
+++ config.py
@@ -1,8 +1,26 @@
 
 GPU_SERVER_HOSTNAME = "http://gpu02.sbms.hku.hk"
-LOCAL_AI_PORT = 49152
+#Local_ai
+#AI_PORT = 49152
+#CHAT_ENDPOINT = "chat/completions"
+
+#vllm
+#AI_PORT = 8000
+#CHAT_ENDPOINT = "v1/chat/completions"
+
+#SGLang
+AI_PORT = 30000
+CHAT_ENDPOINT = "v1/chat/completions"
+
+#AI_MODEL = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+#AI_MODEL = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+AI_MODEL = "neuralmagic/DeepSeek-R1-Distill-Qwen-32B-quantized.w4a16"
+
 ONCOTREE_TXT_FILE_PATH = "ref/oncotree_file.txt"
 GENE_LIST_FILE_PATH = "ref/genes.txt"
+
+
+
 # List of words to remove
 keywords_to_remove = {"cancer", 
                       "advanced", 
